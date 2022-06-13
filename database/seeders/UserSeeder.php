680a78bf79e9af55850ce04d5bf86cc7a16abcb3
Eber Reta <eber.retabaeza@gmail.com>
2022-06-13 00:14:04 -0600

    User Sedeer

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'=>"Eber Reta",
+            'email'=>"eber.retabaeza@gmail.com",
+            'password'=>bcrypt("eber123123"),
+            'email_verified_at'=>date('Y-m-d H:i:s')
+        ]);
+    }
+}
