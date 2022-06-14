4dba98fd19bbcfa1da04e52d370d776f02354ffe
Eber Reta <eber.retabaeza@gmail.com>
2022-06-14 01:12:33 -0600

    Catalogo de Ciudades/Estados

---
+++ database/seeders/StateSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\State;
+use Illuminate\Database\Seeder;
+
+class StateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        State::create(['name'=>'Aguascalientes']);
+        State::create(['name'=>'Baja California']);
+        State::create(['name'=>'Baja California Sur']);
+        State::create(['name'=>'Campeche']);
+        State::create(['name'=>'Chiapas']);
+        State::create(['name'=>'Chihuahua']);
+        State::create(['name'=>'Ciudad de México']);
+        State::create(['name'=>'Coahuila']);
+        State::create(['name'=>'Colima']);
+        State::create(['name'=>'Durango']);
+        State::create(['name'=>'Guanajuato']);
+        State::create(['name'=>'Guerrero']);
+        State::create(['name'=>'Hidalgo']);
+        State::create(['name'=>'Jalisco']);
+        State::create(['name'=>'México']);
+        State::create(['name'=>'Michoacán']);
+        State::create(['name'=>'Morelos']);
+        State::create(['name'=>'Nayarit']);
+        State::create(['name'=>'Nuevo León']);
+        State::create(['name'=>'Oaxaca']);
+        State::create(['name'=>'Puebla']);
+        State::create(['name'=>'Querétaro']);
+        State::create(['name'=>'Quintana Roo']);
+        State::create(['name'=>'San Luis Potosí']);
+        State::create(['name'=>'Sinaloa']);
+        State::create(['name'=>'Sonora']);
+        State::create(['name'=>'Tabasco']);
+        State::create(['name'=>'Tamaulipas']);
+        State::create(['name'=>'Tlaxcala']);
+        State::create(['name'=>'Veracruz']);
+        State::create(['name'=>'Yucatán']);
+        State::create(['name'=>'Zacatecas']);
+    }
+}
